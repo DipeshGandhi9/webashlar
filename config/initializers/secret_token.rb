ad7b76b786b8d2996079b6d1027debd81d801c4f
Dipesh k. Gandhi <dipesh_gandhi9@yahoo.com>
2013-06-03 15:25:13 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webashlar::Application.config.secret_token = '5abb18ee9ebd1e29099d26b26815c999c9707ec9561c5f7f37e9907a37f9145634bfb4dd596c2ae15873e5551336a3fbe81ce069d197998ddb20ab46480785f1'
